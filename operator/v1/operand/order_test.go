878bbdb549e0075d1b648066c00d93cd4ddc7dab
nolancon <conor.nolan@ondat.io>
2023-02-09 16:45:05 +0000

    commonStringsInSlices Unit tests

---
+++ operator/v1/operand/order_test.go
@@ -0,0 +1,45 @@
+package operand
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCommonStringsInSlices(t *testing.T) {
+	testCases := []struct {
+		a, b, want []string
+	}{
+		{
+			[]string{"a", "b", "c"},
+			[]string{"c", "b", "a"},
+			[]string{"a", "b", "c"},
+		},
+		{
+			[]string{"a", "b", "c"},
+			[]string{"d", "e", "f"},
+			[]string{},
+		},
+		{
+			[]string{"apple", "banana", "cherry"},
+			[]string{"cherry", "apple"},
+			[]string{"apple", "cherry"},
+		},
+		{
+			[]string{},
+			[]string{},
+			[]string{},
+		},
+		{
+			[]string{"dog", "cat", "bird"},
+			[]string{},
+			[]string{},
+		},
+	}
+
+	for _, tc := range testCases {
+		got := commonStringsInSlices(tc.a, tc.b)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("commonStringsInSlices(%v, %v) = %v; want %v", tc.a, tc.b, got, tc.want)
+		}
+	}
+}
